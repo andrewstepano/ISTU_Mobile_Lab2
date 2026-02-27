b53abaf40b5d0cb11e15d9c8da7922d572b0b97c
Andrew Stepano <125278750+andrewstepano@users.noreply.github.com>
2026-02-27 17:25:23 +0400

    initial: add Artwork.kt and ArtworkData.kt

---
+++ app/src/main/java/com/example/istu_mobile_lab2/ArtworkData.kt
@@ -0,0 +1,31 @@
+package com.example.istu_mobile_lab2
+
+object ArtworkData {
+    val artworks = listOf(
+        Artwork(
+            imageResId = R.drawable.picture1,
+            titleResId = R.string.picture1_title,
+            authorResId = R.string.picture1_author
+        ),
+        Artwork(
+            imageResId = R.drawable.picture2,
+            titleResId = R.string.picture2_title,
+            authorResId = R.string.picture2_author
+        ),
+        Artwork(
+            imageResId = R.drawable.picture3,
+            titleResId = R.string.picture3_title,
+            authorResId = R.string.picture3_author
+        ),
+        Artwork(
+            imageResId = R.drawable.picture4,
+            titleResId = R.string.picture4_title,
+            authorResId = R.string.picture4_author
+        ),
+        Artwork(
+            imageResId = R.drawable.picture5,
+            titleResId = R.string.picture5_title,
+            authorResId = R.string.picture5_author
+        )
+    )
+}
